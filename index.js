7c8ab287c987e82f8308e110e3b1de781051b42c
Ilia Loginov <i.loginow@outlook.com>
2019-10-15 19:27:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ directive.debounce = debounce
 function directive (el, binding) {
   if (binding.value !== binding.oldValue) { // change debounce only if interval has changed
     el.oninput = directive.debounce(function (evt) {
-      el.dispatchEvent(new Event('change'))
+      el.dispatchEvent(new Event('input'))
     }, parseInt(binding.value) || 500)
   }
 }
